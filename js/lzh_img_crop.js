4247ce6a00aa758ebee17350e4bfdb12293d9b1a
lzh <lczxhf@sina.com>
2016-04-06 20:35:22 +0800

    add selection move

--- js/lzh_img_crop.js
+++ js/lzh_img_crop.js
@@ -47,63 +47,95 @@ LzhImgCrop.prototype.drawImg = function(){
 
 LzhImgCrop.prototype.drawPreImg = function(){
 	// this.preview_ctx.clearRect(this.x,this.y,this.preview_width,this.preview_height)
-	var reduce_x = this.selection.x1 - this.selection.x2
-	var reduce_y = this.selection.y1 - this.selection.y2
-	var start_x = 0;
-	var start_y = 0;
-	if (reduce_x<=0){
-		start_x = this.selection.x1
-	}else{
-		start_x = this.selection.x2
-	}
-	if(reduce_y<=0){
-		start_y = this.selection.y1
-	}else{
-		start_y = this.selection.y2
-	}
 
-	this.preview_ctx.drawImage(this.image,start_x,start_y,Math.abs(reduce_x),Math.abs(reduce_y),this.x,this.y,Math.abs(reduce_x),Math.abs(reduce_y))
+	// var reduce_x = this.selection.x1 - this.selection.x2
+	// var reduce_y = this.selection.y1 - this.selection.y2
+	// var start_x = 0;
+	// var start_y = 0;
+	// if (reduce_x<=0){
+	// 	start_x = this.selection.x1
+	// }else{
+	// 	start_x = this.selection.x2
+	// }
+	// if(reduce_y<=0){
+	// 	start_y = this.selection.y1
+	// }else{
+	// 	start_y = this.selection.y2
+	// }
+
+	this.preview_ctx.drawImage(this.image,this.selection.start_x,this.selection.start_y,Math.abs(this.selection.reduce_x),Math.abs(this.selection.reduce_y),this.x,this.y,Math.abs(this.selection.reduce_x),Math.abs(this.selection.reduce_y))
 }
 
+LzhImgCrop.prototype.MoveDrawPreImg = function(){
+
+}
 
 LzhImgCrop.prototype.CanvasMouseUp = function(e){
 	console.log("up")
-	if(crop_obj.mousedown){
+	if(crop_obj.mousedown && crop_obj.selection.is_change_position){
+		crop_obj.selection.is_change_position = false ;
 		crop_obj.mousedown = false;
-		crop_obj.selection.x2 = e.offsetX;
-		crop_obj.selection.y2 = e.offsetY;
-		if (crop_obj.is_preview){ 
-			crop_obj.drawPreImg();
-		}
-		crop_obj.selection.is_select = true ;
-		crop_obj.selection.drawRect();
+		// crop_obj.selection.x2 = e.offsetX;
+		// crop_obj.selection.y2 = e.offsetY;
+		// if (crop_obj.is_preview){ 
+		// 	crop_obj.drawPreImg();
+		// }
+		 crop_obj.selection.is_select = true;
+		// crop_obj.selection.drawRect();
+	}else if(crop_obj.mousedown){
+		crop_obj.mousedown = false;
+		crop_obj.selection.is_select = true;
 	}
 }
 
 LzhImgCrop.prototype.CanvasMouseDown = function(e){
-	console.log("down")
+	// console.log("down")
 	
-	if(crop_obj.selection.is_select){
+	if(crop_obj.selection.is_select && crop_obj.selection.IsDownInside(e)){
+		console.log("down_move")
+		 crop_obj.selection.RecordMoveInfo(e);
+	}else if(crop_obj.selection.is_select){
+		console.log("down_clear")
 		crop_obj.selection.ClearSelection();
-	}else{
-
-	}
-		crop_obj.mousedown = true;
 		crop_obj.selection.x1 = e.offsetX;
 		crop_obj.selection.y1 = e.offsetY;
+	}else{
+		console.log("down")
+		crop_obj.selection.x1 = e.offsetX;
+		crop_obj.selection.y1 = e.offsetY;
+	}
+		crop_obj.mousedown = true;
+		
 }
 
 LzhImgCrop.prototype.CanvasMouseMove = function(e){
-	console.log("move")
-	if(crop_obj.mousedown){
+	
+	// var reduce_x = 0;
+	// var reduce_y = 0;
+	var draw = false;
+	
+	if(crop_obj.mousedown && crop_obj.selection.is_change_position){
+		console.log("move_change")
+			crop_obj.selection.move_x2 = e.offsetX;
+			crop_obj.selection.move_y2 = e.offsetY;
+			crop_obj.selection.CountDistance()
+			crop_obj.selection.Move()
+			draw = true
+			// reduce_x = crop_obj.selection.move_x1 - crop_obj.selection.move_x2
+			// reduce_y = crop_obj.selection.move_y1 - crop_obj.selection.move_y2
+	}else if(crop_obj.mousedown){
+		console.log("move_select")
 		crop_obj.selection.x2 = e.offsetX;
 		crop_obj.selection.y2 = e.offsetY;
-		
-		crop_obj.selection.ClearSelection();
-		if(crop_obj.is_preview){
-			crop_obj.drawPreImg();
-		}
-		crop_obj.selection.drawRect();
+		crop_obj.selection.CountDistance()
+		draw = true
+	}
+	if (draw){
+	crop_obj.selection.ClearSelection();
+	if(crop_obj.is_preview){
+		crop_obj.drawPreImg();
+	}
+	crop_obj.selection.drawRect();
 	}
 }
 
@@ -136,6 +168,17 @@ function Selection(x1,y1,x2,y2){
 	this.x2 = x2;
 	this.y1 = y1;
 	this.y2 = y2;
+	this.reduce_x = 0;
+	this.reduce_y = 0;
+	this.start_x = 0;
+	this.start_y = 0;
+	this.end_x = 0;
+	this.end_y = 0;
+	this.move_x1 = 0;
+	this.move_x2 = 0;
+	this.move_y1 = 0;
+	this.move_y2 = 0;
+	this.is_change_position = false;
 	this.is_select = false;
 	this.canvas = document.getElementById(crop_obj.can_cover);
 	this.ctx=this.canvas.getContext("2d");
@@ -157,21 +200,21 @@ Selection.prototype.ClearSelection = function(){
 }
 
 Selection.prototype.drawRect = function(){
-	var reduce_x = this.x1 - this.x2
-	var reduce_y = this.y1 - this.y2
-	var start_x = 0;
-	var start_y = 0;
-	if (reduce_x<=0){
-		start_x = this.x1
-	}else{
-		start_x = this.x2
-	}
-	if(reduce_y<=0){
-		start_y = this.y1
-	}else{
-		start_y = this.y2
+	// var reduce_x = this.x1 - this.x2
+	// var reduce_y = this.y1 - this.y2
+	// var start_x = 0;
+	// var start_y = 0;
+	// if (reduce_x<=0){
+	// 	start_x = this.x1
+	// }else{
+	// 	start_x = this.x2
+	// }
+	// if(reduce_y<=0){
+	// 	start_y = this.y1
+	// }else{
+	// 	start_y = this.y2
 		
-	}
+	// }
 	// this.ctx.beginPath();
 	// this.ctx.moveTo(start_x,start_y);
 	// this.ctx.lineTo(start_x,end_y);
@@ -181,7 +224,51 @@ Selection.prototype.drawRect = function(){
 	// this.ctx.stroke();
 	// this.ctx.fillStyle="green";
 	// this.ctx.fill();
-	this.ctx.clearRect(start_x,start_y,Math.abs(reduce_x),Math.abs(reduce_y))	
+	this.ctx.clearRect(this.start_x,this.start_y,Math.abs(this.reduce_x),Math.abs(this.reduce_y))	
 }
+Selection.prototype.CountDistance = function(){
+	this.reduce_x = this.x1 - this.x2
+	this.reduce_y = this.y1 - this.y2
+	
+	if (this.reduce_x<=0){
+		this.start_x = this.x1
+		this.end_x = this.x2
+	}else{
+		this.start_x = this.x2
+		this.end_x = this.x1
+	}
+	if(this.reduce_y<=0){
+		this.start_y = this.y1
+		this.end_y = this.y2
+	}else{
+		this.start_y = this.y2
+		this.end_y = this.y1
+		
+	}
+}
+Selection.prototype.IsDownInside = function(e){
+	if (e.offsetX > this.start_x && e.offsetX < this.end_x && e.offsetY > this.start_y && e.offsetY < this.end_y){
+		return true;
+	}
+	return false;
+}
+Selection.prototype.RecordMoveInfo = function(e){
+	this.is_change_position = true;
+	this.move_x1 = e.offsetX;
+	this.move_y1 = e.offsetY;
+}
+Selection.prototype.Move = function(){
+	var reduce_x = this.move_x1 - this.move_x2
+	var reduce_y = this.move_y1 - this.move_y2
+	this.x1 -= reduce_x
+	this.x2 -= reduce_x
+	this.y1 -= reduce_y
+	this.y2 -= reduce_y
+	this.move_x1 = this.move_x2
+	this.move_y1 = this.move_y2
+
+}
+
+
 
 
